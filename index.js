7e9d6be478c127cd96603fe169e7fa40213e55d3
Davide Melis <davidemelis@Davides-MacBook-Pro.local>
2019-10-21 11:46:42 +0100

    index.js and package.js fix

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 import React from 'react'
 
-export { Avatar } from './avatar'
-export { Badge } from './badge'
-export { Button } from './button'
-export { CollapsableView } from './collapsableView'
-export { Icon } from './icon'
-export { Image } from './image'
-export { Logo } from './logo'
+import Avatar from './avatar'
+import Badge from './badge'
+import Button from './button'
+import CollapsableView from './collapsableView'
+import Icon from './icon'
+import Image from './image'
+import Logo from './logo'
 // export { Modal } from './modal'
-export { RadioButton } from './radioButton'
-export { Slider } from './slider'
-export { Switch } from './switch'
-export { Text } from './text'
-export { ToggleButton, ToggleIcon } from './toggle'
-export { View } from './view'
+import RadioButton from './radioButton'
+import Slider from './slider'
+import Switch from './switch'
+import Text from './text'
+import { ToggleButton, ToggleIcon } from './toggle'
+import View from './view'
+
+module.exports = {Avatar, Badge, Button, CollapsableView, Icon, Image, Logo, RadioButton, Slider, Switch, Text, ToggleButton, ToggleIcon, View}
+// Export React, plus some native additions.
+// module.exports = {
+//     // Components
+//     get Avatar() {
+//         return require('./avatar')
+//     },
+//     get Badge() {
+//         return require('./badge')
+//     },
+//     get Button() {
+//         return require('./button')
+//     },
+//     get CollapsableView() {
+//         return require('./collapsableView')
+//     },
+//     get Icon() {
+//         return require('./icon')
+//     },
+//     get Image() {
+//         return require('./image')
+//     },
+//     get Logo() {
+//         return require('./logo')
+//     },
+//     // get Modal() {
+//     //     return require('./modal')
+//     // },
+//     get RadioButton() {
+//         return require('./radioButton')
+//     },
+//     get Slider() {
+//         return require('./slider')
+//     },
+//     get Switch() {
+//         return require('./switch')
+//     },
+//     get Text() {
+//         return require('./text')
+//     },
+//     get ToggleButton() {
+//         return require('./toggle')
+//     },
+//     get ToggleIcon() {
+//         return require('./toggle')
+//     },
+//     get View() {
+//         return require('./view')
+//     }
+// }
