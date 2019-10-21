e97defbd5a8634e342da89497147f38412df9aa4
Davide Melis <davidemelis@Davides-MacBook-Pro.local>
2019-10-21 17:49:00 +0100

    improved index.js and package.json + README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# @ccrash/react-native-atoms
+> A collection of ready to use Atoms for atomic design projects
+
+# Atoms included in the module
+
+* Avatar
+* Badge
+* Button
+* CollapsableView
+* Icon
+* Image
+* Logo
+* Modal
+* RadioButton
+* Slider
+* Switch
+* Text
+* ToggleButton
+* ToggleIcon
+* View}
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Avatar from './avatar'
 import Badge from './badge'
 import Button from './button'
@@ -7,7 +5,7 @@ import CollapsableView from './collapsableView'
 import Icon from './icon'
 import Image from './image'
 import Logo from './logo'
-// export { Modal } from './modal'
+export { Modal } from './modal'
 import RadioButton from './radioButton'
 import Slider from './slider'
 import Switch from './switch'
@@ -15,53 +13,4 @@ import Text from './text'
 import { ToggleButton, ToggleIcon } from './toggle'
 import View from './view'
 
-module.exports = {Avatar, Badge, Button, CollapsableView, Icon, Image, Logo, RadioButton, Slider, Switch, Text, ToggleButton, ToggleIcon, View}
-// Export React, plus some native additions.
-// module.exports = {
-//     // Components
-//     get Avatar() {
-//         return require('./avatar')
-//     },
-//     get Badge() {
-//         return require('./badge')
-//     },
-//     get Button() {
-//         return require('./button')
-//     },
-//     get CollapsableView() {
-//         return require('./collapsableView')
-//     },
-//     get Icon() {
-//         return require('./icon')
-//     },
-//     get Image() {
-//         return require('./image')
-//     },
-//     get Logo() {
-//         return require('./logo')
-//     },
-//     // get Modal() {
-//     //     return require('./modal')
-//     // },
-//     get RadioButton() {
-//         return require('./radioButton')
-//     },
-//     get Slider() {
-//         return require('./slider')
-//     },
-//     get Switch() {
-//         return require('./switch')
-//     },
-//     get Text() {
-//         return require('./text')
-//     },
-//     get ToggleButton() {
-//         return require('./toggle')
-//     },
-//     get ToggleIcon() {
-//         return require('./toggle')
-//     },
-//     get View() {
-//         return require('./view')
-//     }
-// }
+module.exports = {Avatar, Badge, Button, CollapsableView, Icon, Image, Logo, Modal, RadioButton, Slider, Switch, Text, ToggleButton, ToggleIcon, View}
